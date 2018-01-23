f76aae59e5fdbc03c152dfdbe15580841e8474e0
dfvillar <daniel.vit11@gmail.com>
2018-01-23 00:09:38 -0600

    483 - Word Scramble

--- current/Main.java
+++ current/Main.java
@@ -1,7 +1,34 @@
 
+import java.util.*;
+import java.io.*;
 
-
+/*
+ *  483 - Word Scramble
+ */
 public class Main{
-	
-	
+	public static void main(String... args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = null;
+				
+		while( (line = br.readLine()) != null ){
+			char[] chrs = line.toCharArray();
+			int ln = chrs.length;
+			Deque<Character> stack = new ArrayDeque<Character>();
+			for(int i=0; i<ln; i++){
+				if(chrs[i] != ' '){
+					stack.addFirst(chrs[i]);
+				}else{
+					int size = stack.size();
+					while(size-- > 0)
+						System.out.print(stack.removeFirst());
+					System.out.print(" ");	
+				}										
+			}
+			int size = stack.size();
+				while(size-- > 0)
+					System.out.print(stack.removeFirst());	
+			System.out.println();			
+		}
+		
+	}	
 }
