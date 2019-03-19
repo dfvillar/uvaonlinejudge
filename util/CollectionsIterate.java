13046f2ad78c438795afeb14fae44d1fc8e74fc3
dfvillar <daniel.vit11@gmail.com>
2019-03-18 23:18:29 -0600

    Update

--- util/CollectionsIterate.java
+++ util/CollectionsIterate.java
@@ -74,6 +74,47 @@ class CollectionsIterate{
         while (itr.hasNext()){
 			System.out.print(itr.next() + " ");
         }
+        System.out.println(" ");
+        
+        //HashMap
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        map.put(4, "d");
+        
+        for(Map.Entry<Integer, String> entry : map.entrySet()){
+			System.out.print("Key " + entry.getKey() + " value " + entry.getValue() + " ");
+		}
+		System.out.println(" ");
+		
+		
+		 //TreeMap
+        Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+        treeMap.put(1, "a");
+        treeMap.put(2, "b");
+        treeMap.put(3, "c");
+        treeMap.put(4, "d");
+        
+        for(Map.Entry<Integer, String> entry : treeMap.entrySet()){
+			System.out.print("Key " + entry.getKey() + " value " + entry.getValue() + " ");
+		}
+		System.out.println(" ");
+		
+		//TreeSet
+		Set<Integer> treeSet = new TreeSet<Integer>();
+		treeSet.add(1);
+		treeSet.add(2);
+		treeSet.add(3);
+		treeSet.add(4);
+		treeSet.add(5);
+		
+		Iterator treeIterator = treeSet.iterator();
+		
+		while(treeIterator.hasNext()){
+			System.out.print(treeIterator.next() + " ");
+		}
+		
         
         
         
